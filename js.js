4654adab2a4e92614500eab1b146325db8f32c68
Milan Rajbanshi <146459933+MilanRB1@users.noreply.github.com>
2024-01-07 20:44:19 +0545

    g
    g

--- js.js
+++ js.js
@@ -6,26 +6,12 @@ menu.onclick = ()=>{
     navbar.classList.toggle('active');
 }
 
-let themeToggler = document.querySelector('.theme-toggler');
-let toggleBtn = document.querySelector('.toggle-btn');
-
-toggleBtn.onclick = ()=>{
-    themeToggler.classList.toggle('active');
-}
 
 window.onscroll = ()=>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
-    themeToggler.classList.remove('active');
 }
 
-document.querySelectorAll('.theme-toggler .theme-btn').forEach(buttons =>{
-
-    buttons.onclick = () =>{
-        let color = buttons.style.background;
-        document.querySelector(':root').style.setProperty('--main-color',color);
-    }
-});
 
 var swiper = new Swiper(".home-slider", {
     effect: "coverflow",
